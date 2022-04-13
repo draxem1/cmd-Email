987a0ff68c26de471a7a79727e3b2840187858d2
Nick Legato <nicklegato@gmail.com>
2022-04-13 11:51:55 -0400

    added clear screen/refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 /**************************/
+use std::process::Command;
 use std::io;
+use std::thread;
+use std::time::Duration;
 
 #[derive(Debug)]
 #[allow(dead_code)]
@@ -17,8 +20,10 @@ pub trait Default{
 pub trait New{
 	type Input;
 
-	fn new(&self, _: Self::Input, _: Self::Input,
-					 _: Self::Input, _: Self::Input) -> Self;
+	fn new(&self, _: Self::Input, 
+				_: Self::Input,
+				_: Self::Input, 
+				_: Self::Input) -> Self;
 }
 
 pub trait Getter<T> {
@@ -52,8 +57,10 @@ impl <T>Getter<T> for Draft<T>
 impl <T>New for Draft<T> {
 	type Input = T;
 
-	fn new(&self, sender: Self::Input, reciever: Self::Input,
-					 subject: Self::Input, message: Self::Input) -> Self{
+	fn new(&self, sender: Self::Input, 
+					reciever: Self::Input,
+					subject: Self::Input,
+					message: Self::Input) -> Self{
 		Self {
 			sender,
 			reciever,
@@ -69,6 +76,7 @@ pub fn set_flag<'a, U>(run: &str, email: &U)
 	where U: Getter<&'a str>{
 		match run {
 			"-n" => new_draft(&email.get_sender(), &email.get_reciever()),
+			//"-r" => retrieve_saved_draft(),
 				_=> (),
 		}
 }
@@ -88,24 +96,69 @@ pub fn read_line() -> String{
 //Refactor into another file
 //needs seperate func with editing options of message body
 pub fn new_draft(sender: &str, reciever: &str){
-
+	let call = || -> String{
+		read_line()
+	};
 	let draft = Draft::default();
+	clear_screen();
 
 	println!("Subject: ");
-	let sub = String::from(read_line().trim());
-
-	println!("Message: ");
-	let mut body = String::new();
-
-	while !body.contains("-S"){
-		if body.contains("-CL") {
-			body.clear();
-		} else {
-			body.push_str(read_line().trim());
-		}
-	}
-
+	let sub = String::from(call());
+	let body = edited_message(call);
 	let draft = draft.new(sender, reciever, &sub, &body);
 
 	println!("\n\n{:?}", draft);
+}
+
+/*******************************
+ * --CL: erase "message"
+ * --D: Done
+ * 
+ * **************************/
+ #[allow(unused_variables)]
+pub fn edited_message<F>(call: F) -> String
+	where F: Fn() -> String{
+	let mut body = String::new();
+
+	println!("\nMESSAGE:");
+
+	while !body.contains("--D") {
+		if body.contains("--CL") {
+            body.clear();
+
+		} else {
+			body.push_str(&call());
+		}
+	}
+	body.trim().to_string()
+}
+
+/*****
+pub fn back_a_line(message: &str) -> String{
+	let s = String::from(message);
+	let mut m: Vec<&str> = s.split("\r").collect();
+	let mut string = String::new();
+
+	m.pop();
+
+	for i in m {
+		string.push_str(i);
+	}
+
+	string
+}
+****/
+
+//Used threads to give next print macro time
+pub fn clear_screen(){
+	let handle = thread::spawn(|| {
+		Command::new("cmd")
+			.args(["/C","cls"])
+			.spawn()
+			.expect("Couldn't clear!!");
+		thread::sleep(Duration::from_millis(1));
+	});
+
+	handle.join().unwrap();
+	thread::sleep(Duration::from_millis(1));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,38 @@
 //send email messages from the command line
 //cargo run -- address option
+
 use cli_email::Default;
 use std::env;
+use std::process;
+use std::result::Result;
 //use std::process::Command; // runs os commands
 use cli_email::Draft;
 
 fn main() {
-    let input: Vec<String> = env::args().collect();
+    let input = all_args_exist().unwrap_or_else(|err| {
+        eprintln!("Err: {}", err);
+        process::exit(0);
+    });
 
     let mut draft = Draft::default();
+
     draft.sender = &input[1];
     draft.reciever = &input[2];
-    let option = &input[3];
 
+    let option = &input[3];
     cli_email::set_flag(option, &draft);
 }
 
+fn all_args_exist() -> Result<Vec<String>, &'static str> 
+    {
+        let input: Vec<String> = env::args().collect();
+
+        if input.len() < 4 {
+            return Err("Not enough arguments");
+        }
+
+        Ok(input)
+}
 /*************
 Command::new("cmd")
         .args(["/C" , "npm"])
