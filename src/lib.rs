6544b73a81479bbad1a1205a830ff480e0867262
Nick Legato <nicklegato@gmail.com>
2022-04-13 05:34:49 -0400

    Fixed generics and lifetimes for setting and copy Draft

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 /**************************/
 use std::io;
 
+#[derive(Debug)]
+#[allow(dead_code)]
 pub struct Draft<T>{
 	pub sender: T,
 	pub reciever: T,
@@ -12,11 +14,19 @@ pub trait Default{
 	fn default() -> Self;
 }
 
+pub trait New{
+	type Input;
+
+	fn new(&self, _: Self::Input, _: Self::Input,
+					 _: Self::Input, _: Self::Input) -> Self;
+}
+
 pub trait Getter<T> {
 	fn get_sender(&self) -> T;
 	fn get_reciever(&self) -> T;
 }
 
+//Sets Generic T as lifetime &str
 impl<'a> Default for Draft<&str>{
 	fn default() -> Self{
 		Self {
@@ -39,15 +49,31 @@ impl <T>Getter<T> for Draft<T>
 	}
 }
 
-//do command from args and input
-pub fn set_flag<U, T>(run: &str, email: &U)
-	where U: Getter<T>{
+impl <T>New for Draft<T> {
+	type Input = T;
+
+	fn new(&self, sender: Self::Input, reciever: Self::Input,
+					 subject: Self::Input, message: Self::Input) -> Self{
+		Self {
+			sender,
+			reciever,
+			subject,
+			message,
+		}
+	}
+}
+
+//Because T become &str lifetime parameter takes place of T
+//Refactor into another file
+pub fn set_flag<'a, U>(run: &str, email: &U)
+	where U: Getter<&'a str>{
 		match run {
 			"-n" => new_draft(&email.get_sender(), &email.get_reciever()),
 				_=> (),
 		}
 }
 
+//Can be a caller func in another file
 pub fn read_line() -> String{
     let mut line = String::new();
 
@@ -59,12 +85,14 @@ pub fn read_line() -> String{
 }
 
 
-pub fn new_draft<T>(_sender: T, _reciever: T){
+//Refactor into another file
+//needs seperate func with editing options of message body
+pub fn new_draft(sender: &str, reciever: &str){
 
-	let mut _draft = Draft::default();
+	let draft = Draft::default();
 
 	println!("Subject: ");
-	let _sub = String::from(read_line().trim());
+	let sub = String::from(read_line().trim());
 
 	println!("Message: ");
 	let mut body = String::new();
@@ -76,4 +104,8 @@ pub fn new_draft<T>(_sender: T, _reciever: T){
 			body.push_str(read_line().trim());
 		}
 	}
+
+	let draft = draft.new(sender, reciever, &sub, &body);
+
+	println!("\n\n{:?}", draft);
 }
